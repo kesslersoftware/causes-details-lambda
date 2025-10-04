71bc49235d6789b777cda19070df89a616a21156
schnarbiemeows <dylnksslr53@gmail.com>
2025-10-04 09:30:13 -0400

    obtaining > 90% code coverage

--- src/main/java/com/boycottpro/causes/GetCauseDetailsHandler.java
+++ src/main/java/com/boycottpro/causes/GetCauseDetailsHandler.java
@@ -8,6 +8,7 @@ import com.boycottpro.models.Causes;
 import com.boycottpro.models.ResponseMessage;
 import com.boycottpro.utilities.CausesUtility;
 import com.boycottpro.utilities.JwtUtility;
+import com.boycottpro.utilities.Logger;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
@@ -35,28 +36,37 @@ public class GetCauseDetailsHandler implements
     @Override
     public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent event, Context context) {
         String sub = null;
+        int lineNum = 39;
         try {
             sub = JwtUtility.getSubFromRestEvent(event);
-            if (sub == null) return response(401, Map.of("message", "Unauthorized"));
+            if (sub == null) {
+            Logger.error(43, sub, "user is Unauthorized");
+            return response(401, Map.of("message", "Unauthorized"));
+            }
+            lineNum = 46;
             Map<String, String> pathParams = event.getPathParameters();
             String causeId = (pathParams != null) ? pathParams.get("cause_id") : null;
             if (causeId == null || causeId.isEmpty()) {
+                Logger.error(50, sub, "cause_id not present");
                 ResponseMessage message = new ResponseMessage(400,
                         "sorry, there was an error processing your request",
                         "cause_id not present");
                 return response(400,message);
             }
+            lineNum = 56;
             Causes causes = getCausesById(causeId);
+            lineNum = 58;
             if (causes == null) {
+                Logger.error(60, sub, "causes record could not be found");
                 ResponseMessage message = new ResponseMessage(500,
                         "sorry, there was an error processing your request",
                         "causes record could not be found");
                 return response(500,message);
             }
-            System.out.println("RESPONSE = " + causes);
+            lineNum = 66;
             return response(200,causes);
         } catch (Exception e) {
-            System.out.println(e.getMessage() + " for user " + sub);
+            Logger.error(lineNum, sub, e.getMessage());
             return response(500,Map.of("error", "Unexpected server error: " + e.getMessage()) );
         }
     }
